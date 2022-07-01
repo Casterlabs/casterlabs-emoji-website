f1dddb01487771a04f96fca6a81dbde219458d24
Ender <33337309+e3ndr@users.noreply.github.com>
2022-06-30 19:30:32 -0500

    Update SvelteKit.
    Now using the fancy new named layouts.

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,8 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+		prerender: { default: true},
+		adapter: adapter()
 	}
 };
 
